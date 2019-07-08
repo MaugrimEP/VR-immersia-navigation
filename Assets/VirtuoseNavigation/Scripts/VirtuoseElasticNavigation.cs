829c68b0e5b53deecb4a74713c2127102dabf347
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-07-08 10:26:03 +0200

    always attached virtuose pose

--- Assets/VirtuoseNavigation/Scripts/VirtuoseElasticNavigation.cs
+++ Assets/VirtuoseNavigation/Scripts/VirtuoseElasticNavigation.cs
@@ -6,12 +6,11 @@ public class VirtuoseElasticNavigation : MonoBehaviour
 {
     public VirtuoseManager vm;
 
+    private bool attached = false;
     private Vector3 attachedPosition;
     private Quaternion attachedRotation;
 
-    public float TranslationMultiplier = 0.1f;
     public float TranslationTreshold = 0.01f;
-    public float RotationMultiplier = 0.1f;
     public float RotationTreshold = 0.05f;
 
     [Header("SphereParameters")]
@@ -19,36 +18,34 @@ public class VirtuoseElasticNavigation : MonoBehaviour
     public float SphereRunAddRadius;
     public float ForceFactor;
 
+    private void Start()
+    {
+        attached = false;
+    }
+
     void Update()
     {
         if (!vm.Arm.IsConnected) return;
 
-        if (!vm.IsButtonPressed()) (attachedPosition, attachedRotation) = vm.Virtuose.Pose;
+        if (!attached)
+        {
+            (attachedPosition, attachedRotation) = vm.Virtuose.Pose;
+            attached = true;
+        }
 
         Vector3 differencePos = vm.Virtuose.Pose.position - attachedPosition;
 
-        if (!vm.IsButtonPressed())
+        Vector3 forces = -differencePos * ForceFactor;
+        vm.Virtuose.virtAddForce = (forces, torques: Vector3.zero);
+
+        if (differencePos.magnitude > SphereWalkRadius + SphereRunAddRadius)
+        {
+            Vector3 PositionOnSphere = differencePos.GetPointInSphere(SphereWalkRadius + SphereRunAddRadius);
+            vm.Virtuose.Pose = (attachedPosition + PositionOnSphere, vm.Virtuose.Pose.rotation);
+        }
+        else
         {
             vm.Virtuose.SetPoseIdentity();
-            vm.Virtuose.SetSpeedIdentity();
-            vm.Virtuose.virtAddForceIdentity();
-            return;
-        }
-
-        if (vm.IsButtonPressed())
-        {
-            Vector3 forces = -differencePos * ForceFactor;
-            vm.Virtuose.virtAddForce = (forces, torques: Vector3.zero);
-
-            if (differencePos.magnitude > SphereWalkRadius + SphereRunAddRadius)
-            {
-                Vector3 PositionOnSphere = differencePos.GetPointInSphere(SphereWalkRadius + SphereRunAddRadius);
-                vm.Virtuose.Pose = (attachedPosition + PositionOnSphere, vm.Virtuose.Pose.rotation);
-            }
-            else
-            {
-                vm.Virtuose.SetPoseIdentity();
-            }
         }
     }
 
@@ -61,12 +58,8 @@ public class VirtuoseElasticNavigation : MonoBehaviour
         if (!vm.Arm.IsConnected || !vm.IsButtonPressed()) return Vector3.zero;
 
         Vector3 differencePos = vm.Virtuose.Pose.position - attachedPosition;
-        //clamp the value between [-1,1], should clamp by projecting on the sphere ?
         Vector3 translation = (differencePos / (SphereWalkRadius + SphereRunAddRadius)).Clamp(-1f, 1f);
         translation = translation.Treshold(TranslationTreshold, 0f);
-
-        Debug.Log($"Input : {translation}");
-
         return translation;
     }
 
