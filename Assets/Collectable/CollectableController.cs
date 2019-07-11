c5aa33d1aa70aa860e197199315a58321be851fe
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-07-11 15:54:05 +0200

    add collectables elements

---
+++ Assets/Collectable/CollectableController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour
+{
+    public Color succesColor;
+    public List<ParticleSystem> particleSystems;
+
+    private List<Color> startColor;
+
+    private bool isSucced;
+
+    private void Start()
+    {
+        startColor = new List<Color>();
+        foreach (ParticleSystem ps in particleSystems)
+            startColor.Add(ps.startColor);
+        isSucced = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isSucced)
+        {
+            Succed();
+            isSucced = true;
+        }
+    }
+
+    private void Succed()
+    {
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            float alpha = ps.startColor.a;
+            Color newColor = succesColor;
+            newColor.a = alpha;
+            ps.startColor = newColor;
+        }
+    }
+
+    public void ResetColor()
+    {
+        for(int i = 0; i < particleSystems.Count; ++i)
+        {
+            ParticleSystem ps = particleSystems[i];
+            ps.startColor = startColor[i];
+        }
+        isSucced = false;
+    }
+}
---
+++ Assets/Collectable/ResetCollectableArea/ResetAOEController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetAOEController : MonoBehaviour
+{
+    public List<CollectableController> CollectableControllers;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (CollectableController cc in CollectableControllers)
+            cc.ResetColor();
+    }
+
+}
