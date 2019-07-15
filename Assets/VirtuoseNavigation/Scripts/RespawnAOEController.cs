b795283c436c9f9e7e2a911eb7b9ccaf5394ecfa
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-07-15 15:53:16 +0200

    Adding map area and respawn on exit

---
+++ Assets/VirtuoseNavigation/Scripts/RespawnAOEController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnAOEController : MonoBehaviour
+{
+    public Transform RespawnPoint;
+    public GameObject Player;
+
+    private void OnTriggerExit(Collider _)
+    {
+        Player.transform.position = RespawnPoint.position;
+    }
+}
