1a4e7f575a3d66fb28e3dad93972fc449ea2522a
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-07-19 12:16:25 +0200

    adding player killer element

---
+++ Assets/PlayerKiller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour
+{
+    private GameObject Player;
+    private Vector3 RespawnPoint;
+
+    private void Start()
+    {
+        Player = GameObject.Find("Character");
+        RespawnPoint = GameObject.Find("respawnPoint").transform.position;
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider _)
+    {
+        Player.transform.position = RespawnPoint;
+    }
+}
